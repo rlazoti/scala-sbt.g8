a3591900f6045602b591039ec4966b3064a242f0
Rodrigo Lazoti <rlazoti@corp.hotwords.com>
2013-07-19 15:04:51 -0300

    Settings of plugin stb-package-dist

--- src/main/g8/project/build.scala
+++ src/main/g8/project/build.scala
@@ -13,6 +13,11 @@ object AppBuilder extends Build {
 
   lazy val app = Project("$name$", file("."))
     .settings(appSettings : _*)
+    .settings(GitProject.gitSettings : _*)
+    .settings(BuildProperties.newSettings : _*)
+    .settings(PackageDist.newSettings : _*)
+    .settings(ReleaseManagement.newSettings : _*)
+    .settings(VersionManagement.newSettings : _*)
     .settings(libraryDependencies ++= appDependencies)
 
 }

3ae2ce24124533c5c80be44ddf6ae8993808bfc6
Rodrigo Lazoti <rlazoti@corp.hotwords.com>
2013-07-19 12:18:48 -0300

    Added junit dependency

--- src/main/g8/project/dependencies.scala
+++ src/main/g8/project/dependencies.scala
@@ -3,9 +3,11 @@ import sbt._
 object Dependencies {
 
   val scalatest = "org.scalatest" %% "scalatest" % "$scalatest_version$"
+  val junit = "junit" % "junit" % "4.11"
 
   val appDependencies = Seq(
-    scalatest % "test"
+    scalatest % "test",
+    junit % "test"
   )
 
 }
